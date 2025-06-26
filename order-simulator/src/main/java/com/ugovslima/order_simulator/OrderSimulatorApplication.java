73fc2d4a874bb754fdbc68fad5dd723cfb289dd6
uvsl <ugovslima@gmail.com>
2025-06-26 14:30:35 -0300

    Initial commit

---
+++ order-simulator/src/main/java/com/ugovslima/order_simulator/OrderSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.ugovslima.order_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSimulatorApplication.class, args);
+	}
+
+}
---
+++ order-simulator/src/test/java/com/ugovslima/order_simulator/OrderSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugovslima.order_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
