7e352c9ef082ddff980284c2b3956f9a4adc1148
uvsl <ugovslima@gmail.com>
2025-06-28 22:16:19 -0300

    feat(api-gateway): add a gateway and route to catalog service

---
+++ API-gateway/src/main/java/com/ugovslima/API/gateway/config/GatewayConfig.java
@@ -0,0 +1,19 @@
+package com.ugovslima.API.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("catalog-microservice", r -> r
+                        .path("/produtos/**")
+                        .uri("lb://catalog-microservice"))
+                .build();
+    }
+}
